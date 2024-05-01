2c763fc05efdd488896ff3bf20a6a35bd00c20b1
pib0student <223180030@student.unaki.ac.id>
2024-05-01 13:37:35 +0700

    update klik detail card

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # website-gbi-bethany
-pembuatan website gereja GBI Bethany Pangkalan Bun dan hosting di Vercel 
+
+pembuatan website gereja GBI Bethany Pangkalan Bun dan hosting di Vercel
+
+# update-1
+
+membuat card dapat di klik menuju page detail pada section 'acara dan pelayanan'
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,3 +17,51 @@ window.onscroll = () => {
     }
   });
 };
+
+// Menangkap card dengan ID "linkDetailWartaGereja"
+let btnDetailWartaGereja = document.getElementById("linkDetailWartaGereja");
+// Menambahkan event listener untuk mengarahkan pengguna ke halaman tujuan saat tombol ditekan
+btnDetailWartaGereja.addEventListener("click", function () {
+  // Ganti "pages/warta_gereja.html" dengan URL halaman yang ingin Anda arahkan
+  window.location.href = "pages/warta_gereja.html";
+});
+
+// Menangkap card dengan ID "linkDetailPosPi"
+let btnDetailPosPi = document.getElementById("linkDetailPosPi");
+// Menambahkan event listener untuk mengarahkan pengguna ke halaman tujuan saat tombol ditekan
+btnDetailPosPi.addEventListener("click", function () {
+  // Ganti "pages/pos_pi.html" dengan URL halaman yang ingin Anda arahkan
+  window.location.href = "pages/pos_pi.html";
+});
+
+// Menangkap card dengan ID "linkDetailDoaMlm"
+let btnDetailDoaMlm = document.getElementById("linkDetailDoaMlm");
+// Menambahkan event listener untuk mengarahkan pengguna ke halaman tujuan saat tombol ditekan
+btnDetailDoaMlm.addEventListener("click", function () {
+  // Ganti "pages/doa_malam.html" dengan URL halaman yang ingin Anda arahkan
+  window.location.href = "pages/doa_malam.html";
+});
+
+// Menangkap card dengan ID "linkDetailWanitaGBI"
+let btnDetailWanitaGBI = document.getElementById("linkDetailWanitaGBI");
+// Menambahkan event listener untuk mengarahkan pengguna ke halaman tujuan saat tombol ditekan
+btnDetailWanitaGBI.addEventListener("click", function () {
+  // Ganti "pages/wanita_gbi.html" dengan URL halaman yang ingin Anda arahkan
+  window.location.href = "pages/wanita_gbi.html";
+});
+
+// Menangkap card dengan ID "linkDetailWanitaGBI"
+let btnDetailGenB = document.getElementById("linkDetailGenB");
+// Menambahkan event listener untuk mengarahkan pengguna ke halaman tujuan saat tombol ditekan
+btnDetailGenB.addEventListener("click", function () {
+  // Ganti "pages/gen_b.html" dengan URL halaman yang ingin Anda arahkan
+  window.location.href = "pages/gen_b.html";
+});
+
+// Menangkap card dengan ID "linkDetailBethanyKids"
+let btnDetailBethanyKids = document.getElementById("linkDetailBethanyKids");
+// Menambahkan event listener untuk mengarahkan pengguna ke halaman tujuan saat tombol ditekan
+btnDetailBethanyKids.addEventListener("click", function () {
+  // Ganti "pages/bethany_kids.html" dengan URL halaman yang ingin Anda arahkan
+  window.location.href = "pages/bethany_kids.html";
+});
